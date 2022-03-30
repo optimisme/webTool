e7d3f10968ada6cc3ff5c3947fea160376518433
Titus Appletosh <titusappletosh@Tituss-MacBook-Pro.local>
2022-03-30 19:29:40 +0200

    Added animation shot

--- public/examples/scrollMovement.js
+++ public/examples/scrollMovement.js
@@ -2,11 +2,11 @@ window.addEventListener("scroll", (evt) => { scrollMovement(evt) })
 
 function scrollMovement (evt) {
     if (evt && evt.type == "scroll") {
-        let refs = document.querySelectorAll('[data-scroll="true"]')
+        let refs = document.querySelectorAll('[data-scroll-movement="true"]')
         for (let cnt = 0; cnt < refs.length; cnt = cnt + 1) {
             let ref = refs[cnt]
             let refBounds = ref.getBoundingClientRect()
-            let safeY = (!ref.hasAttribute("data-safeY")) ? 0 : parseFloat(ref.getAttribute("data-safeY"))
+            let safeY = (!ref.hasAttribute("data-scroll-safe-y")) ? 0 : parseFloat(ref.getAttribute("data-scroll-safe-y"))
             let limitY = refBounds.y + refBounds.height - safeY
             let actionHeight = document.documentElement.clientHeight + refBounds.height - (safeY * 2)
             let percentage = 0
