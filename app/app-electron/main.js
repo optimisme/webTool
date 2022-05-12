af0555c99a22e08aa336dcaee9c554274e79e453
Albert Palacios Jimenez <albertpalaciosjimenez@Alberts-Mac-mini.local>
2022-05-12 08:52:46 +0200

    Try to remove menu from win and linux

--- app/app-electron/main.js
+++ app/app-electron/main.js
@@ -27,11 +27,13 @@ function createWindow () {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
-
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+  app.on("browser-window-created", (e, win) => {
+      mainWindow.removeMenu();
   })
 })
 
